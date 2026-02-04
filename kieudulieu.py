304456925bcec239e2f61d45595c8ba907cd9081
DESKTOP-0A3K9BM\DELL <s232543@nctu.edu.vn>
2026-02-04 20:45:17 +0700

    first commit

---
+++ kieudulieu.py
@@ -0,0 +1,12 @@
+int_num = int('10')
+float_num = float("10.5")
+str_num = None
+
+# kiểm tra kiểu dữ liệu 
+print(isinstance(int_num, int))
+print(isinstance(float_num, float))
+print(isinstance(str_num, str))
+# 
+print(int_num is None)
+print(str_num is None)
+# none kiểm tra ra true hoặc là false true là không có dữ liệu còn false có thể có hoặc không có dữ liệu
---
+++ tinhcong.py
@@ -0,0 +1,4 @@
+firt_num = 10
+second_num= 30
+result = firt_num + second_num
+print(result)
